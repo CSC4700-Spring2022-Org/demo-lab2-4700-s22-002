367173b1452d98c8b82dc17869b8e47edce03d83
ms-O <kristin.obermyer@villanova.edu>
2022-04-07 08:28:28 -0400

    Add test and implementation for convert()

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class CurrencyConverter {
 
@@ -6,6 +8,11 @@ public class CurrencyConverter {
 
     public static void main(String[] args) {
         System.out.println("Hello CurrencyConverter");
+    }
+
+    public BigDecimal convert(BigDecimal originalAmount, BigDecimal rate) {
+        BigDecimal result = originalAmount.multiply(rate).setScale(2, RoundingMode.HALF_UP);
+        return result;
     }
 
 }
